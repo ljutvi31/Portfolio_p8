3967d8cccbdb189d9bdef2a8f40b2cde04257a31
Harry Ljutvi <ljutvi.h@gmail.com>
2025-02-20 20:05:50 +0100

    Normalize line endings

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajoute tous les fichiers JS/TS/JSX/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
